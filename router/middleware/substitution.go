81e3cfc864dedcd26b474bc0ccae7e7adfb3971b
a.tselishchev <a.tselishchev@containerum.io>
2017-09-29 08:37:39 +0000

    Fix substitutions & misc changes.

--- router/middleware/substitution.go
+++ router/middleware/substitution.go
@@ -5,6 +5,8 @@ import (
 	"encoding/json"
 	"fmt"
 
+	"bitbucket.org/exonch/kube-api/utils"
+
 	"k8s.io/api/apps/v1beta1"
 	"k8s.io/api/core/v1"
 
@@ -23,116 +25,92 @@ func (re userReplacement) zero() bool {
 	return re.To == "" || re.From == ""
 }
 
-// NOTE: обработчик выполняется на прямом и обратном ходе рекурсии.
-func SubstitutionsFromHeaders(c *gin.Context) {
-	var reps map[string][]userReplacement // http header name -> array of userReplacement
+// SubstitutionsFromHeadersFor takes an object from the context by the key objctxkey,
+// if it is there, does the substitutions as specified in HTTP request headers
+// and puts it back into the context by the same key. Parameter after controls whether
+// the substitutions are performed after all other handlers in this chain are run.
+// In such case, the substitutions are reversed
+func SubstitutionsFromHeadersFor(objctxkey string, after bool) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		var reps map[string][]userReplacement // http header name -> array of userReplacement
 
-	reps = make(map[string][]userReplacement)
-	reps["x-user-namespace"] = nil
-	reps["x-user-volume"] = nil
+		reps = make(map[string][]userReplacement)
+		reps["x-user-namespace"] = nil
+		reps["x-user-volume"] = nil
 
-	// find, decode & validate data
-	for hdr, repArray := range reps {
-		var b64 string
-		if b64 = c.Request.Header.Get(hdr); b64 == "" {
-			continue
+		// find, decode & validate data
+		for hdr, repArray := range reps {
+			var b64 string
+			if b64 = c.Request.Header.Get(hdr); b64 == "" {
+				continue
+			}
+			jsn, err := base64.StdEncoding.DecodeString(b64)
+			if err != nil {
+				logrus.Errorf("cannot b64-decode header %q: %v", hdr, err)
+				c.AbortWithStatus(400)
+				c.Set("responseObject", map[string]string{
+					"error": fmt.Sprintf("cannot decode %q: %v", hdr, err),
+				})
+				return
+			}
+			err = json.Unmarshal(jsn, &repArray)
+			if err != nil {
+				logrus.Errorf("cannot parse json from header %q: %v", hdr, err)
+				c.AbortWithStatus(400)
+				c.Set("responseObject", map[string]string{
+					"error": fmt.Sprintf("cannot parse %q: %v", hdr, err),
+				})
+				return
+			}
+			reps[hdr] = repArray
 		}
-		jsn, err := base64.StdEncoding.DecodeString(b64)
-		if err != nil {
-			logrus.Errorf("cannot b64-decode header %q: %v", hdr, err)
-			c.AbortWithStatus(400)
-			c.Set("responseObject", map[string]string{
-				"error": fmt.Sprintf("cannot decode %q: %v", hdr, err),
-			})
-			return
-		}
-		err = json.Unmarshal(jsn, &repArray)
-		if err != nil {
-			logrus.Errorf("cannot parse json from header %q: %v", hdr, err)
-			c.AbortWithStatus(400)
-			c.Set("responseObject", map[string]string{
-				"error": fmt.Sprintf("cannot parse %q: %v", hdr, err),
-			})
-			return
-		}
-		reps[hdr] = repArray
-	}
 
-	// >>> sub INCOMING data >>>
-	if obj, ok := c.Get("requestObject"); ok {
-		switch objtyped := obj.(type) {
-		case *v1.Namespace:
-			subNamespace(objtyped, reps)
-		case *v1.NamespaceList:
-			for i := range objtyped.Items {
-				subNamespace(&objtyped.Items[i], reps)
-			}
-		case *v1beta1.Deployment:
-			subDeployment(objtyped, reps)
-		case *v1beta1.DeploymentList:
-			for i := range objtyped.Items {
-				subDeployment(&objtyped.Items[i], reps)
-			}
-		case *v1.Service:
-			subService(objtyped, reps)
-		case *v1.ServiceList:
-			for i := range objtyped.Items {
-				subService(&objtyped.Items[i], reps)
-			}
-		case *v1.Endpoints:
-			subEndpoints(objtyped, reps)
-		case *v1.EndpointsList:
-			for i := range objtyped.Items {
-				subEndpoints(&objtyped.Items[i], reps)
-			}
-		default:
-			logrus.Panicf("refusing to handle incoming type %T", obj)
-		}
-		//c.Set("requestObject", obj) // no need as the types are pointers
-	}
+		if after {
+			c.Next()
 
-	// reverse the from-to parts
-	for _, repArray := range reps {
-		for i := range repArray {
-			repArray[i].From, repArray[i].To = repArray[i].To, repArray[i].From
+			// reverse the from-to parts
+			for _, repArray := range reps {
+				for i := range repArray {
+					repArray[i].From, repArray[i].To = repArray[i].To, repArray[i].From
+				}
+			}
 		}
-	}
 
-	// invoke further handerls in the chain
-	c.Next()
-
-	// <<< sub OUTGOING data <<<
-	if obj, ok := c.Get("responseObject"); ok {
-		switch objtyped := obj.(type) {
-		case *v1.Namespace:
-			subNamespace(objtyped, reps)
-		case *v1.NamespaceList:
-			for i := range objtyped.Items {
-				subNamespace(&objtyped.Items[i], reps)
+		if obj, ok := c.Get(objctxkey); ok {
+			switch objtyped := obj.(type) {
+			case *v1.Namespace:
+				subNamespace(objtyped, reps)
+			case *v1.NamespaceList:
+				for i := range objtyped.Items {
+					subNamespace(&objtyped.Items[i], reps)
+				}
+			case *v1beta1.Deployment:
+				subDeployment(objtyped, reps)
+			case *v1beta1.DeploymentList:
+				for i := range objtyped.Items {
+					subDeployment(&objtyped.Items[i], reps)
+				}
+			case *v1.Service:
+				subService(objtyped, reps)
+			case *v1.ServiceList:
+				for i := range objtyped.Items {
+					subService(&objtyped.Items[i], reps)
+				}
+			case *v1.Endpoints:
+				subEndpoints(objtyped, reps)
+			case *v1.EndpointsList:
+				for i := range objtyped.Items {
+					subEndpoints(&objtyped.Items[i], reps)
+				}
+			default:
+				utils.Log(c).
+					WithField("handler", "SubstitutionsFromHeadersFor").
+					WithField("handler-objctxkey", objctxkey).
+					WithField("handler-after", after).
+					Infof("refusing to handle type %T", obj)
 			}
-		case *v1beta1.Deployment:
-			subDeployment(objtyped, reps)
-		case *v1beta1.DeploymentList:
-			for i := range objtyped.Items {
-				subDeployment(&objtyped.Items[i], reps)
-			}
-		case *v1.Service:
-			subService(objtyped, reps)
-		case *v1.ServiceList:
-			for i := range objtyped.Items {
-				subService(&objtyped.Items[i], reps)
-			}
-		case *v1.Endpoints:
-			subEndpoints(objtyped, reps)
-		case *v1.EndpointsList:
-			for i := range objtyped.Items {
-				subEndpoints(&objtyped.Items[i], reps)
-			}
-		case map[string]string:
-		default:
-			logrus.Panicf("refusing to handle type %T", obj)
+			//c.Set("requestObject", obj) // no need as the types are pointers
 		}
-		//c.Set("responseObject", obj) // no need as the types are pointers
 	}
 }
 
