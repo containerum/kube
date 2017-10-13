bb35d4d1489a614a749338e1188227c680e7bfa3
a.tselishchev <a.tselishchev@containerum.io>
2017-10-13 11:24:31 +0000

    save progress

--- access/roles.go
+++ access/roles.go
@@ -1,15 +1,32 @@
 package access
 
+import (
+	"k8s.io/api/apps/v1beta1"
+	"k8s.io/api/core/v1"
+)
+
 type HTTPHeaders struct {
 	Namespace []model.UserData
 	Volume    []model.UserData
 }
 
-func (hhdr HTTPHeaders) CanCreateObject() bool {
-}
-
-func (hhdr HTTPHeaders) CanCreateNamespace() bool {
+func (h HTTPHeaders) CanCreateObject(x interface{}) bool {
+	switch xt := x.(type) {
+	case *v1.Namespace:
+	case *v1.Service:
+	case *v1.Secret:
+	case *v1beta1.Deployment:
+	}
 }
 
 func (h HTTPHeaders) CanUseVolume(volLabel string) bool {
+}
+
+func (h HTTPHeaders) CanDeleteObject(x interface{}) bool {
+	switch xt := x.(type) {
+	case *v1.Namespace:
+	case *v1.Service:
+	case *v1.Secret:
+	case *v1beta1.Deployment:
+	}
 }
