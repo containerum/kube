5e325114ff8cc4811749c6a6cddf0a487099a487
a.tselishchev <a.tselishchev@containerum.io>
2017-10-16 16:20:28 +0000

    Compose authorization matrix.

--- access/middleware.go
+++ access/middleware.go
@@ -1,9 +1,13 @@
 package access
 
 import (
+	"fmt"
+	"os"
+
 	"github.com/gin-gonic/gin"
 )
 
+// Required permission for the action on the object.
 type Perm string
 
 const (
@@ -14,8 +18,18 @@ const (
 	Edit   Perm = "edit"
 )
 
-// accessMap : objtype → Perm → role → true or false
-var accessMap = make(map[string]map[Perm]map[string]bool)
+// User's access level for this object.
+type AccessLevel string
+
+const (
+	LvlOwner      AccessLevel = "owner"
+	LvlRead       AccessLevel = "read"
+	LvlWrite      AccessLevel = "write"
+	LvlReadDelete AccessLevel = "read-delete"
+)
+
+// accessMap : objtype → Perm → role → (true|false)
+var accessMap = make(map[string]map[Perm]map[AccessLevel]bool)
 
 func init() {
 	objtypes := []string{
@@ -33,19 +47,61 @@ func init() {
 		Read,
 		Edit,
 	}
-	accessLevels := []string{
-		"owner",
-		"read",
-		"write",
-		"read-delete",
+	accessLevels := []AccessLevel{
+		LvlOwner,
+		LvlRead,
+		LvlWrite,
+		LvlReadDelete,
 	}
 
 	for _, x := range objtypes {
-		accessMap[x] = make(map[Perm]map[string]bool)
+		accessMap[x] = make(map[Perm]map[AccessLevel]bool)
 		for _, y := range perms {
-			accessMap[x][y] = make(map[string]bool)
+			accessMap[x][y] = make(map[AccessLevel]bool)
 			for _, z := range accessLevels {
 				accessMap[x][y][z] = false
+			}
+		}
+	}
+
+	for _, lvl := range []AccessLevel{LvlRead, LvlWrite, LvlOwner, LvlReadDelete} {
+		for tp := range accessMap {
+			for perm := range accessMap[tp] {
+				if lvl == LvlRead {
+					if perm == List || perm == Read {
+						accessMap[tp][perm][lvl] = true
+					} else {
+						accessMap[tp][perm][lvl] = false
+					}
+				}
+
+				if lvl == LvlWrite {
+					accessMap[tp][perm][lvl] = accessMap[tp][perm][LvlRead]
+					if perm == Edit {
+						accessMap[tp][perm][lvl] = true
+					}
+				}
+
+				if lvl == LvlOwner {
+					accessMap[tp][perm][lvl] = true
+				}
+
+				if lvl == LvlReadDelete {
+					accessMap[tp][perm][lvl] = accessMap[tp][perm][LvlRead]
+					if perm == Delete {
+						accessMap[tp][perm][lvl] = true
+					}
+				}
+			}
+		}
+	}
+
+	if os.Getenv("GIN_MODE") != "release" {
+		for k1 := range accessMap {
+			for k2 := range accessMap[k1] {
+				for k3 := range accessMap[k1][k2] {
+					fmt.Printf("%s %s %s %t\n", k1, k2, k3, accessMap[k1][k2][k3])
+				}
 			}
 		}
 	}
