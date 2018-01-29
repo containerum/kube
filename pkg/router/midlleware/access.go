02d7b583800b36c39e59b0efc402ac89f2d14902
Konstantin <kfeofantov@yandex.ru>
2018-01-29 23:27:40 +0300

    add access middle

--- pkg/router/midlleware/access.go
+++ pkg/router/midlleware/access.go
@@ -6,10 +6,21 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type AccessLevel string
+
+const (
+	levelOwner      AccessLevel = "owner"
+	levelRead       AccessLevel = "read"
+	levelWrite      AccessLevel = "write"
+	levelReadDelete AccessLevel = "read-delete"
+	levelNone       AccessLevel = ""
+)
+
 func IsAdmin() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		if role := c.MustGet(UserRole).(string); role != "admin" {
+		if role := c.GetHeader(userRoleXHeader); role != "admin" {
 			c.AbortWithStatus(http.StatusForbidden)
+			return
 		}
 	}
 }
