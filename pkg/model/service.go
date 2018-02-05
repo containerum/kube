80979e6f35ddf0f2779020986a753feb66d561f2
paul <petrukhin.paul@gmail.com>
2018-02-05 14:13:17 +0300

    user service struct

--- pkg/model/service.go
+++ pkg/model/service.go
@@ -16,13 +16,42 @@ type ServicePort struct {
 	Protocol   kubeCoreV1.Protocol `json:"protocol"`
 }
 
-// ServicePortFromNativePort converts native
+// ServicePortFromNativeKubePort converts native
 // cubernetes service port representation to user friendly ServicePort struct
-func ServicePortFromNativePort(nativePort kubeCoreV1.ServicePort) ServicePort {
+func ServicePortFromNativeKubePort(nativePort kubeCoreV1.ServicePort) ServicePort {
 	return ServicePort{
 		Name:       nativePort.Name,
 		Port:       uint32(nativePort.Port),
 		TargetPort: nativePort.TargetPort,
 		Protocol:   nativePort.Protocol,
 	}
+}
+
+// Service is an user friendly kebernetes service representation
+// CreatedAt is an unix timestamp
+type Service struct {
+	CreatedAt int64                  `json:"created_at"`
+	Deploy    string                 `json:"deploy"`
+	IP        []string               `json:"ip"`
+	Domain    string                 `json:"domain, omitempty"`
+	Type      kubeCoreV1.ServiceType `json:"type"`
+	Ports     []ServicePort          `json:"ports"`
+}
+
+// ServiceFromNativeKubeService creates
+// user friendly service representation
+func ServiceFromNativeKubeService(native *kubeCoreV1.Service) *Service {
+	service := &Service{
+		CreatedAt: native.GetCreationTimestamp().Unix(),
+		Deploy:    native.GetObjectMeta().GetLabels()["app"], // TODO: check if app key doesn't exists!
+		IP:        native.Spec.ExternalIPs,
+		Domain:    "", // TODO :!
+		Type:      native.Spec.Type,
+		Ports:     make([]ServicePort, 0, 1),
+	}
+	for _, nativePort := range native.Spec.Ports {
+		service.Ports = append(service.Ports,
+			ServicePortFromNativeKubePort(nativePort))
+	}
+	return service
 }
