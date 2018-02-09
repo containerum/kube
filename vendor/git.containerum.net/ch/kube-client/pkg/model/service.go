9c7e41b160bbfa90dd1c5b3490dddc29fa849249
Ilia Donchenko <iliad@me.com>
2018-02-09 09:20:16 +0300

    Updated vendors

--- vendor/git.containerum.net/ch/kube-client/pkg/model/service.go
+++ vendor/git.containerum.net/ch/kube-client/pkg/model/service.go
@@ -7,24 +7,20 @@ const (
 	TCP Protocol = "TCP"
 )
 
-// ServicePort is an user friendly service port representation
-// Name is DNS_LABEL
-// TargetPort is an int32 or IANA_SVC_NAME
-// Protocol is TCP or UDP
-type ServicePort struct {
-	Name       string   `json:"name"`
-	Port       uint32   `json:"port"`
-	TargetPort uint32   `json:"target_port"`
-	Protocol   Protocol `json:"protocol"`
+type Service struct {
+	Name      string        `json:"name" binding:"required"`
+	Owner     string        `json:"owner" binding:"required"`
+	CreatedAt int64         `json:"created_at, omitempty"`
+	Deploy    string        `json:"deploy" binding:"required"`
+	IP        []string      `json:"ip" omitempty"`
+	Domain    string        `json:"domain, omitempty"`
+	Type      string        `json:"type" binding:"required"`
+	Ports     []ServicePort `json:"ports, omitempty"`
 }
 
-// Service is an user friendly kebernetes service representation
-// CreatedAt is an unix timestamp
-type Service struct {
-	CreatedAt int64         `json:"created_at"`
-	Deploy    string        `json:"deploy"`
-	IP        []string      `json:"ip"`
-	Domain    string        `json:"domain, omitempty"`
-	Type      string        `json:"type"`
-	Ports     []ServicePort `json:"ports"`
+type ServicePort struct {
+	Name       string   `json:"name" binding:"required"`
+	Port       int      `json:"port" binding:"required"`
+	TargetPort int      `json:"target_port, omitempty"`
+	Protocol   Protocol `json:"protocol" binding:"required"`
 }
