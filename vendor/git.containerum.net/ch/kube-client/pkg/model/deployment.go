7dd9790f2bdb631a28593da8a54d0842044401f6
Ilia Donchenko <iliad@me.com>
2018-02-27 15:01:33 +0300

    Vendors

--- vendor/git.containerum.net/ch/kube-client/pkg/model/deployment.go
+++ vendor/git.containerum.net/ch/kube-client/pkg/model/deployment.go
@@ -1,15 +1,7 @@
 package model
 
-type Deployment struct {
-	Name            string             `json:"name" binding:"required"`
-	Owner           *string            `json:"owner,omitempty" binding:"required"`
-	Replicas        int                `json:"replicas" binding:"required"`
-	Containers      []Container        `json:"containers" binding:"required"`
-	ImagePullSecret *map[string]string `json:"image_pull_secret,omitempty"`
-	Status          *DeploymentStatus  `json:"status,omitempty"`
-	Hostname        *string            `json:"hostname,omitempty"`
-}
-
+// DeploymentStatus -- represents deployment status
+// CreatedAt and UpdatedAt -- Unix timestamps
 type DeploymentStatus struct {
 	CreatedAt           int64 `json:"created_at"`
 	UpdatedAt           int64 `json:"updated_at"`
@@ -20,6 +12,49 @@ type DeploymentStatus struct {
 	UpdatedReplicas     int   `json:"updated_replicas"`
 }
 
+// UpdateReplicas -- contains new number of replicas
 type UpdateReplicas struct {
-	Replicas int `json:"replicas" binding:"required"`
+	Replicas int `json:"replicas"`
+}
+
+// Deployment --
+type Deployment struct {
+	Status     *DeploymentStatus `json:"status,omitempty"`
+	Containers []Container       `json:"containers"`
+	Labels     map[string]string `json:"labels,omitempty"`
+	Name       string            `json:"name"`
+	Replicas   int               `json:"replicas"`
+}
+
+// Container --
+type Container struct {
+	Image        string            `json:"image"`
+	Name         string            `json:"name"`
+	Limits       Resource          `json:"limits"`
+	Env          []Env             `json:"env,omitempty"`
+	Commands     []string          `json:"commands,omitempty"`
+	Ports        []ContainerPort   `json:"ports,omitempty"`
+	VolumeMounts []ContainerVolume `json:"volume_mounts,omitempty"`
+	ConfigMaps   []ContainerVolume `json:"config_maps,omitempty"`
+}
+
+// Env -- represents key value pair of enviroment variable
+type Env struct {
+	Value string `json:"value"`
+	Name  string `json:"name"`
+}
+
+// ContainerPort --
+type ContainerPort struct {
+	Name     string   `json:"name"`
+	Port     int      `json:"port"`
+	Protocol Protocol `json:"protocol"`
+}
+
+// ContainerVolume --
+type ContainerVolume struct {
+	Name      string  `json:"name"`
+	Mode      *string `json:"mode,omitempty"`
+	MountPath string  `json:"mount_path"`
+	SubPath   *string `json:"sub_path,omitempty"`
 }
