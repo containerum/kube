3634158341296f90739f6e793d971c731f2c56f1
Margarita <margo.tuleninova@gmail.com>
2018-03-16 16:47:27 +0300

    fix ingress access

--- pkg/router/router.go
+++ pkg/router/router.go
@@ -79,9 +79,9 @@ func initRoutes(e *gin.Engine) {
 		{
 			ingress.GET("", m.ReadAccess, h.GetIngressList)
 			ingress.GET("/:ingress", m.ReadAccess, h.GetIngress)
-			ingress.POST("", h.CreateIngress)
-			ingress.PUT("/:ingress", h.UpdateIngress)
-			ingress.DELETE("/:ingress", h.DeleteIngress)
+			ingress.POST("", m.ReadAccess, h.CreateIngress)
+			ingress.PUT("/:ingress", m.ReadAccess, h.UpdateIngress)
+			ingress.DELETE("/:ingress", m.ReadAccess, h.DeleteIngress)
 		}
 
 		endpoint := namespace.Group("/:namespace/endpoints")
