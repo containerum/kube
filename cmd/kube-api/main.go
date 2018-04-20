796bc33b7bb4e7ea41a75a3756c1b074217b71dc
Ilia Donchenko <iliad@me.com>
2018-04-20 17:41:52 +0300

    Display application name, version and flags on start

--- cmd/kube-api/main.go
+++ cmd/kube-api/main.go
@@ -12,6 +12,9 @@ func main() {
 	app.Name = "ch-kube-api-server"
 	app.Usage = "Kube api server for Container Hosting"
 	app.Flags = flags
+
+	fmt.Printf("Starting %v %v\n", app.Name, app.Version)
+
 	app.Action = server
 
 	if err := app.Run(os.Args); err != nil {
