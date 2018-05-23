a7205ab3b75e4c6a43bd323545e53f12b1271613
Ilia Donchenko <iliad@me.com>
2018-05-23 18:01:29 +0300

    Vendor

--- vendor/github.com/containerum/kube-client/pkg/model/volume.go
+++ vendor/github.com/containerum/kube-client/pkg/model/volume.go
@@ -29,9 +29,29 @@ type ResourceUpdateName struct {
 	Label string `json:"label"`
 }
 
-// ResourceUpdateUserAccess -- containes user access data
-//swagger:ignore
-type ResourceUpdateUserAccess struct {
-	Username string `json:"username"`
-	Access   string `json:"access,omitempty"`
+type PersistentVolumeAccessMode string
+
+const (
+	// can be mounted read/write mode to exactly 1 host
+	ReadWriteOnce PersistentVolumeAccessMode = "ReadWriteOnce"
+	// can be mounted in read-only mode to many hosts
+	ReadOnlyMany PersistentVolumeAccessMode = "ReadOnlyMany"
+	// can be mounted in read/write mode to many hosts
+	ReadWriteMany PersistentVolumeAccessMode = "ReadWriteMany"
+)
+
+// PersistentVolumeClaim -- persistent volume claim representation
+//
+//swagger:model
+type PersistentVolumeClaim struct {
+	// required: true
+	Name string `json:"name"`
+	//creation date in RFC3339 format
+	CreatedAt *string `json:"created_at,omitempty"`
+	// required: true
+	StorageClass string `json:"storage_class"`
+	// required: true
+	AccessMode PersistentVolumeAccessMode `json:"access_mode"`
+	// required: true
+	Size uint `json:"size"`
 }
