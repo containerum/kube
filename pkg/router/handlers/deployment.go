f870377a138dffb735f826cad2085076ee02804f
Ilia Donchenko <iliad@me.com>
2018-05-28 12:18:42 +0300

    Don't check persistent volume

--- pkg/router/handlers/deployment.go
+++ pkg/router/handlers/deployment.go
@@ -186,12 +186,11 @@ func CreateDeployment(ctx *gin.Context) {
 		return
 	}
 
-	for _, v := range deploy.Spec.Template.Spec.Volumes {
-		_, err := kube.GetPersistentVolume(namespace, v.PersistentVolumeClaim.ClaimName)
-		if err != nil {
+	/*for _, v := range deploy.Spec.Template.Spec.Volumes {
+			if _, err := kube.GetPersistentVolume(namespace, v.PersistentVolumeClaim.ClaimName); err != nil {
 			//TODO: Do something like check if PersistentVolume exist and in bound state
 		}
-	}
+	}*/
 
 	deployAfter, err := kube.CreateDeployment(deploy)
 	if err != nil {
